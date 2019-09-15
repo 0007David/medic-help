297ee0ab41eccebc45ba40ceee6a9c144fde0500
David <davidtn52@gmail.com>
2019-09-15 16:21:00 +0200

    prueba de apiRESTful

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Test;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+
+	/**
+	 * Metodo que muestra el contenido inicial hacia una vista
+	 * @return view
+	 */
+	public function index(){
+		$saludo = 'Hola Mundo';
+		// $saludo = Test::all();
+		 return $saludo;
+	}
+
+	/**
+	 * Metodo que inserta los datos de un modelo de la BBDD
+	 * @return 
+	 */
+	public function store(Request $request, Test $test){
+	
+	}
+
+	/**
+	 * Metodo que actualiza los datos de un modelo de la BBDD
+	 * @return 
+	 */
+	public function update(Request $request, Test $test){
+
+	
+	}
+
+	/**
+	 * Metodo que muestra los datos de un modelo de la BBDD
+	 * @return 
+	 */
+	public function show(){
+	
+	}
+
+	/**
+	 * Metodo que elimina un dato del modelo de la BBDD
+	 * @return 
+	 */
+	public function destroy(Test $test){
+	
+	}
+
+}
