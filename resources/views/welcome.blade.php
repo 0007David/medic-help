a3e9a3d2b20ac9ecdbb9cd3cabb11e9f8edb45da
David <davidtn52@gmail.com>
2019-09-15 11:52:02 +0200

    commit de prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HOLA MUNDO CON LARAVEL
                 </div>
 
                 <div class="links">
