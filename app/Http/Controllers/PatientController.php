0f459745a55425ffa23a5d91fb4c077c24d6e39c
Jroge <jorgerodrigotorrez@gmail.com>
2019-09-28 14:07:14 -0400

    Prueba de insertar

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Patient;
 use App\Person;
+use DB;
+use Carbon;
 
 class PatientController extends Controller
 {
@@ -15,8 +17,12 @@ class PatientController extends Controller
 	 */
 	public function index(){
 
+		DB::table('employees')->insert([
+			"type"=>1,
+			"created_at"=>Carbon::now()
+		]);
 		$patients = Person::whereHasMorph('peopleable',[Patient::class])->get();
-
+		
 		echo json_encode($patients);
 		
 
